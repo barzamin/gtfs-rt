5facf6d540c4f92a774750b2e482b6aca2cdc5fe
Erin Moon <erin@hecke.rs>
2021-09-19 01:56:58 -0500

    gtfs-rt 0.2: update prost, add serde integration
    all data structures are now `Serialize`able
    
    `# Please enter the commit message for your changes. Lines starting

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
-extern crate prost_build;
+use prost_build::Config;
 
 fn main() {
-    prost_build::compile_protos(&["src/gtfs-realtime.proto"],
-                                &["src/"]).unwrap();
+    Config::new()
+        .type_attribute(".", "#[derive(Serialize)] #[serde(rename_all = \"snake_case\")]")
+        .compile_protos(&["src/gtfs-realtime.proto"],
+                      &["src/"]).unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
-extern crate prost;
 #[macro_use]
 extern crate prost_derive;
+#[macro_use]
+extern crate serde;
 
 // Include the `items` module, which is generated from items.proto.
 include!(concat!(env!("OUT_DIR"), "/transit_realtime.rs"));

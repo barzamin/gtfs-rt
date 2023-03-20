13ff0b1c082fa9b41e91e7f01bdaf6c86c9de9c0
Egor Markov <emarkov@notanotherone.com>
2023-03-20 10:13:48 +0300

    add github action, format code (and make it clippy-clean)

--- examples/dump.rs
+++ examples/dump.rs
@@ -1,13 +1,15 @@
-use std::env;
-use std::fs;
-use std::error::Error;
 use gtfs_rt::FeedMessage;
 use prost::Message;
+use std::env;
+use std::error::Error;
+use std::fs;
 
-fn main() -> Result<(), Box<Error>> {
-    let fname = env::args().nth(1).ok_or("pass path to gtfs-rt protobuf file")?;
+fn main() -> Result<(), Box<dyn Error>> {
+    let fname = env::args()
+        .nth(1)
+        .ok_or("pass path to gtfs-rt protobuf file")?;
     let data = fs::read(fname)?;
-    let message = FeedMessage::decode(data)?;
+    let message = FeedMessage::decode(data.as_slice())?;
     println!("{:#?}", message);
 
     Ok(())
